6e108f23b8d6c0b82fbf3f200669224db0a7a5ba
Colleen Murphy <colleen@puppetlabs.com>
2014-07-11 10:47:18 -0700

    Migrate mysql backend to use openstacklib::db::mysql
    Implements: blueprint commmon-openstack-database-resource
    Change-Id: I5dc293c1b8033000523fae504f789199599cf447

--- spec/classes/keystone_db_mysql_spec.rb
+++ spec/classes/keystone_db_mysql_spec.rb
@@ -13,48 +13,22 @@ describe 'keystone::db::mysql' do
     { :osfamily => 'Debian' }
   end
 
-  let :param_defaults do
+  let :params do
     {
       'password'      => 'keystone_default_password',
-      'dbname'        => 'keystone',
+    }
+  end
+
+  describe 'with only required params' do
+    it { should contain_openstacklib__db__mysql('keystone').with(
       'user'          => 'keystone',
-      'charset'       => 'utf8',
-      'collate'       => 'utf8_unicode_ci',
+      'password_hash' => '*B552157B14BCEDDCEAA06767A012F31BDAA9CE3D',
+      'dbname'        => 'keystone',
       'host'          => '127.0.0.1',
-      'allowed_hosts' => ['127.0.0.%', '192.168.1.%']
-    }
-  end
-
-  [
-    {},
-    {
-      'password' => 'password',
-      'dbname'   => 'not_keystone',
-      'user'     => 'dan',
-      'host'     => '127.0.0.2',
-      'charset'  => 'utf8'
-    }
-  ].each do |p|
-
-    let :params do
-      p
-    end
-
-    let :param_values do
-      param_defaults.merge(p)
-    end
-
-    it { should contain_class('mysql::python') }
-
-    it { should contain_mysql__db(param_values['dbname']).with(
-      'user'     => param_values['user'],
-      'password' => param_values['password'],
-      'host'     => param_values['host'],
-      'charset'  => param_values['charset'],
-      'require'  => 'Class[Mysql::Config]'
+      'charset'       => 'utf8'
     )}
-
   end
+
   describe "overriding allowed_hosts param to array" do
     let :params do
       {
@@ -63,16 +37,6 @@ describe 'keystone::db::mysql' do
       }
     end
 
-    it {should_not contain_keystone__db__mysql__host_access("127.0.0.1").with(
-      :user     => 'keystone',
-      :password => 'keystonepass',
-      :database => 'keystone'
-    )}
-    it {should contain_keystone__db__mysql__host_access("%").with(
-      :user     => 'keystone',
-      :password => 'keystonepass',
-      :database => 'keystone'
-    )}
   end
   describe "overriding allowed_hosts param to string" do
     let :params do
@@ -82,11 +46,6 @@ describe 'keystone::db::mysql' do
       }
     end
 
-    it {should contain_keystone__db__mysql__host_access("192.168.1.1").with(
-      :user     => 'keystone',
-      :password => 'keystonepass2',
-      :database => 'keystone'
-    )}
   end
 
   describe "overriding allowed_hosts param equals to host param " do
@@ -97,11 +56,6 @@ describe 'keystone::db::mysql' do
       }
     end
 
-    it {should_not contain_keystone__db__mysql__host_access("127.0.0.1").with(
-      :user     => 'keystone',
-      :password => 'keystonepass2',
-      :database => 'keystone'
-    )}
   end
 
 end
