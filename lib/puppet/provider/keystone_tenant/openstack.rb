acf3dc6f06b6dcc5af876517c8511a5225e5e3f6
Colleen Murphy <colleen@puppetlabs.com>
2014-11-16 16:23:00 -0800

    Use openstackclient for keystone_tenant
    This patch migrates the keystone_tenant provider to use the universal
    openstack client instead of the keystone client. It uses the openstack
    parent provider in openstacklib to handle multiple authenticating
    methods. The keystone_tenant type uses the openstacklib openstack
    utility to add a new auth parameter to the keystone_tenant type.
    
    This patch also moves functionality for parsing keystone.conf for the
    service token back to the keystone module from openstacklib. It creates
    three tiers of inheritance: Keystone_tenant < Keystone < Openstack, so
    that keystone-specific functionality can stay in keystone.
    
    It also adds a flush method which should help improve performance.
    
    blueprint use-openstackclient-in-module-resources
    
    Change-Id: I2d9a16f334d1e60ebdd36805e2f8d8d2ef82cf39

---
+++ lib/puppet/provider/keystone_tenant/openstack.rb
@@ -0,0 +1,100 @@
+require 'puppet/provider/keystone'
+
+Puppet::Type.type(:keystone_tenant).provide(
+  :openstack,
+  :parent => Puppet::Provider::Keystone
+) do
+
+  desc "Provider to manage keystone tenants/projects."
+
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+  end
+
+  def create
+    properties = []
+    if resource[:enabled] == :true
+      properties << '--enable'
+    elsif resource[:enabled] == :false
+      properties << '--disable'
+    end
+    if resource[:description]
+      properties << '--description'
+      properties << resource[:description]
+    end
+    request('project', 'create', resource[:name], resource[:auth], properties)
+  end
+
+  def exists?
+    ! instance(resource[:name]).empty?
+  end
+
+  def destroy
+    request('project', 'delete', resource[:name], resource[:auth])
+  end
+
+
+  def enabled=(value)
+    @property_flush[:enabled] = value
+  end
+
+  def enabled
+    bool_to_sym(instance(resource[:name])[:enabled])
+  end
+
+
+  def description=(value)
+    @property_flush[:description] = value
+  end
+
+  def description
+    instance(resource[:name])[:description]
+  end
+
+
+  def id
+    instance(resource[:name])[:id]
+  end
+
+  def self.instances
+    list = request('project', 'list', nil, nil, '--long')
+    list.collect do |project|
+      new(
+        :name        => project[:name],
+        :ensure      => :present,
+        :enabled     => project[:enabled].downcase.chomp == 'true' ? true : false,
+        :description => project[:description],
+        :id          => project[:id]
+      )
+    end
+  end
+
+  def instances
+    instances = request('project', 'list', nil, resource[:auth], '--long')
+    instances.collect do |project|
+      {
+        :name        => project[:name],
+        :enabled     => project[:enabled].downcase.chomp == 'true' ? true : false,
+        :description => project[:description],
+        :id          => project[:id]
+      }
+    end
+  end
+
+  def instance(name)
+    @instances ||= instances.select { |instance| instance[:name] == name }.first || {}
+  end
+
+  def flush
+    options = []
+    if @property_flush
+      (options << '--enable') if @property_flush[:enabled] == :true
+      (options << '--disable') if @property_flush[:enabled] == :false
+      # There is a --description flag for the set command, but it does not work if the value is empty
+      (options << '--property' << "description=#{resource[:description]}") if @property_flush[:description]
+      request('project', 'set', resource[:name], resource[:auth], options) unless options.empty?
+    end
+  end
+
+end
--- lib/puppet/type/keystone_tenant.rb
+++ lib/puppet/type/keystone_tenant.rb
@@ -1,38 +1,47 @@
+require 'puppet/util/openstack'
 Puppet::Type.newtype(:keystone_tenant) do
 
-  desc <<-EOT
-    This type can be used to manage
-    keystone tenants.
-
-    This is assumed to be running on the same node
-    as your keystone API server.
-  EOT
+  desc 'This type can be used to manage keystone tenants.'
 
   ensurable
 
   newparam(:name, :namevar => true) do
+    desc 'The name of the tenant.'
     newvalues(/\w+/)
   end
 
   newproperty(:enabled) do
-    newvalues(/(t|T)rue/, /(f|F)alse/)
-    defaultto('True')
+    desc 'Whether the tenant should be enabled. Defaults to true.'
+    newvalues(/(t|T)rue/, /(f|F)alse/, true, false )
+    defaultto(true)
     munge do |value|
-      value.to_s.capitalize
+      value.to_s.downcase.to_sym
     end
   end
 
-  newproperty(:description)
+  newproperty(:description) do
+    desc 'A description of the tenant.'
+    defaultto('')
+  end
 
   newproperty(:id) do
+    desc 'Read-only property of the tenant.'
     validate do |v|
       raise(Puppet::Error, 'This is a read only property')
     end
   end
 
-  # we should not do anything until the keystone service is started
+  # This ensures the service is started and therefore the keystone
+  # config is configured IF we need them for authentication.
+  # If there is no keystone config, authentication credentials
+  # need to come from another source.
   autorequire(:service) do
     ['keystone']
   end
 
+  auth_param_doc=<<EOT
+If no other credentials are present, the provider will search in
+/etc/keystone/keystone.conf for an admin token and auth url.
+EOT
+  Puppet::Util::Openstack.add_openstack_type_methods(self, auth_param_doc)
 end
