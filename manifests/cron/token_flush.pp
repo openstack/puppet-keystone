821cc4ada1f50b5a6c6244cd5c689a467d06d736
François Charlier <francois.charlier@enovance.com>
2014-11-26 18:02:42 +0100

    Allow disabling or delaying the token_flush cron
    Add the max_delay parameter to prevent running all cron jobs at the
    exact same time in case this cron job is installed on more than one
    server. The default is to not wait though.
    
    Add the ensure parameter to allow removing the cron job.
    
    Change-Id: I1cd231305a1ecc642c638af6cef7d591d1e3fe3e
    (cherry picked from commit 8e502087aa2c3a4970fac7a33622072a377e9bdf)

--- manifests/cron/token_flush.pp
+++ manifests/cron/token_flush.pp
@@ -21,6 +21,10 @@
 #
 # === Parameters
 #
+#  [*ensure*]
+#    (optional) Defaults to present.
+#    Valid values are present, absent.
+#
 #  [*minute*]
 #    (optional) Defaults to '1'.
 #
@@ -36,16 +40,30 @@
 #  [*weekday*]
 #    (optional) Defaults to '*'.
 #
+#  [*maxdelay*]
+#    (optional) Seconds. Defaults to 0. Should be a positive integer.
+#    Induces a random delay before running the cronjob to avoid running all
+#    cron jobs at the same time on all hosts this job is configured.
+#
 class keystone::cron::token_flush (
+  $ensure   = present,
   $minute   = 1,
   $hour     = 0,
   $monthday = '*',
   $month    = '*',
   $weekday  = '*',
+  $maxdelay = 0,
 ) {
 
+  if $maxdelay == 0 {
+    $sleep = ''
+  } else {
+    $sleep = "sleep `expr \${RANDOM} \\% ${maxdelay}`; "
+  }
+
   cron { 'keystone-manage token_flush':
-    command     => 'keystone-manage token_flush >>/var/log/keystone/keystone-tokenflush.log 2>&1',
+    ensure      => $ensure,
+    command     => "${sleep}keystone-manage token_flush >>/var/log/keystone/keystone-tokenflush.log 2>&1",
     environment => 'PATH=/bin:/usr/bin:/usr/sbin',
     user        => 'keystone',
     minute      => $minute,
