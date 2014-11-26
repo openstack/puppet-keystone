8e502087aa2c3a4970fac7a33622072a377e9bdf
François Charlier <francois.charlier@enovance.com>
2014-11-26 18:02:42 +0100

    Allow disabling or delaying the token_flush cron
    Add the max_delay parameter to prevent running all cron jobs at the
    exact same time in case this cron job is installed on more than one
    server. The default is to not wait though.
    
    Add the ensure parameter to allow removing the cron job.
    
    Change-Id: I1cd231305a1ecc642c638af6cef7d591d1e3fe3e

--- spec/classes/keystone_cron_token_flush_spec.rb
+++ spec/classes/keystone_cron_token_flush_spec.rb
@@ -6,16 +6,63 @@ describe 'keystone::cron::token_flush' do
     { :osfamily => 'Debian' }
   end
 
-  it 'configures a cron' do
-    should contain_cron('keystone-manage token_flush').with(
-      :command     => 'keystone-manage token_flush >>/var/log/keystone/keystone-tokenflush.log 2>&1',
-      :environment => 'PATH=/bin:/usr/bin:/usr/sbin',
-      :user        => 'keystone',
-      :minute      => 1,
-      :hour        => 0,
-      :monthday    => '*',
-      :month       => '*',
-      :weekday     => '*'
-    )
+  describe 'with default parameters' do
+    it 'configures a cron' do
+      should contain_cron('keystone-manage token_flush').with(
+        :ensure      => 'present',
+        :command     => 'keystone-manage token_flush >>/var/log/keystone/keystone-tokenflush.log 2>&1',
+        :environment => 'PATH=/bin:/usr/bin:/usr/sbin',
+        :user        => 'keystone',
+        :minute      => 1,
+        :hour        => 0,
+        :monthday    => '*',
+        :month       => '*',
+        :weekday     => '*'
+      )
+    end
+  end
+
+  describe 'when specifying a maxdelay param' do
+    let :params do
+      {
+        :maxdelay => 600
+      }
+    end
+
+    it 'configures a cron with delay' do
+      should contain_cron('keystone-manage token_flush').with(
+        :ensure      => 'present',
+        :command     => 'sleep `expr ${RANDOM} \\% 600`; keystone-manage token_flush >>/var/log/keystone/keystone-tokenflush.log 2>&1',
+        :environment => 'PATH=/bin:/usr/bin:/usr/sbin',
+        :user        => 'keystone',
+        :minute      => 1,
+        :hour        => 0,
+        :monthday    => '*',
+        :month       => '*',
+        :weekday     => '*'
+      )
+    end
+  end
+
+  describe 'when specifying a maxdelay param' do
+    let :params do
+      {
+        :ensure => 'absent'
+      }
+    end
+
+    it 'configures a cron with delay' do
+      should contain_cron('keystone-manage token_flush').with(
+        :ensure      => 'absent',
+        :command     => 'keystone-manage token_flush >>/var/log/keystone/keystone-tokenflush.log 2>&1',
+        :environment => 'PATH=/bin:/usr/bin:/usr/sbin',
+        :user        => 'keystone',
+        :minute      => 1,
+        :hour        => 0,
+        :monthday    => '*',
+        :month       => '*',
+        :weekday     => '*'
+      )
+    end
   end
 end
