72a02aa70880bb5cb07d0a2b1e828fffbae5d0ed
Colleen Murphy <colleen@puppetlabs.com>
2014-12-17 15:16:15 -0800

    Use openstackclient for keystone_service
    blueprint use-openstackclient-in-module-resources
    
    Change-Id: Ib889dbc6b13c22f77e73747d0b2fd589d0882ef0

---
+++ spec/unit/provider/keystone_service/openstack_spec.rb
@@ -0,0 +1,102 @@
+require 'puppet'
+require 'spec_helper'
+require 'puppet/provider/keystone_service/openstack'
+
+provider_class = Puppet::Type.type(:keystone_service).provider(:openstack)
+
+describe provider_class do
+
+  describe 'when creating a service' do
+
+    let(:service_attrs) do
+      {
+        :name         => 'foo',
+        :description  => 'foo',
+        :ensure       => 'present',
+        :type         => 'foo',
+        :auth         => {
+          'username'    => 'test',
+          'password'    => 'abc123',
+          'tenant_name' => 'foo',
+          'auth_url'    => 'http://127.0.0.1:5000/v2.0',
+        }
+      }
+    end
+
+    let(:resource) do
+      Puppet::Type::Keystone_service.new(service_attrs)
+    end
+
+    let(:provider) do
+      provider_class.new(resource)
+    end
+
+    describe '#create' do
+      it 'creates a service' do
+        provider.class.stubs(:openstack)
+                      .with('service', 'list', '--quiet', '--format', 'csv', [['--long', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
+                      .returns('"ID","Name","Type","Description"
+"1cb05cfed7c24279be884ba4f6520262","foo","foo","foo"
+')
+        provider.class.stubs(:openstack)
+                      .with('service', 'create', [['foo', '--description', 'foo', '--type', 'foo', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
+        provider.create
+        expect(provider.exists?).to be_truthy
+      end
+    end
+
+    describe '#destroy' do
+      it 'destroys a service' do
+        provider.class.stubs(:openstack)
+                      .with('service', 'list', '--quiet', '--format', 'csv', [['--long', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
+                      .returns('"ID","Name","Type","Description"')
+        provider.class.stubs(:openstack)
+                      .with('service', 'delete', [['foo', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
+        provider.destroy
+        expect(provider.exists?).to be_falsey
+      end
+
+    end
+
+    describe '#exists' do
+      context 'when service exists' do
+
+        subject(:response) do
+          provider.class.stubs(:openstack)
+                        .with('service', 'list', '--quiet', '--format', 'csv', [['--long', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
+                        .returns('"ID","Name","Type","Description"
+"1cb05cfed7c24279be884ba4f6520262","foo","foo","foo"
+')
+          response = provider.exists?
+        end
+
+        it { is_expected.to be_truthy }
+      end
+
+      context 'when service does not exist' do
+
+        subject(:response) do
+          provider.class.stubs(:openstack)
+                        .with('service', 'list', '--quiet', '--format', 'csv', [['--long', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
+                        .returns('"ID","Name","Type","Description"')
+          response = provider.exists?
+        end
+
+        it { is_expected.to be_falsey }
+      end
+    end
+
+    describe '#instances' do
+      it 'finds every service' do
+        provider.class.stubs(:openstack)
+                      .with('service', 'list', '--quiet', '--format', 'csv', [['--long', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
+                      .returns('"ID","Name","Type","Description"
+"1cb05cfed7c24279be884ba4f6520262","foo","foo","foo"
+')
+        instances = provider.instances
+        expect(instances.count).to eq(1)
+      end
+    end
+
+  end
+end
