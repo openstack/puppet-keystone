8ae1e83d99cfb4885a215cf70eea1108c6329c67
Colleen Murphy <colleen@puppetlabs.com>
2015-02-25 17:38:25 -0800

    Remove warnings from openstack responses
    Puppet's `commands` and `has_command` methods are not able to separate
    stdout and stderr. When the openstack command prints out unexpected
    warnings that have no bearing on our computation, we need to ignore it,
    otherwise the CSV module will fail to parse it. This patch filters the
    total response produced by the openstack command into only lines that
    match a CSV-like regular expression with at least two fields.
    
    If a legitimate error is produced that causes the command to return
    failure, puppet catches it during the command execution and raises a
    Puppet::Execution error, so this change will not affect its ability to
    detect legitimate failures and report on them.
    
    Change-Id: I4f3a8b1ddf6b06dcbf194dd66408c71d4da6d861
    (cherry picked from commit 24effa1e360b1eacec7fbfdfed339ba6e639f7fd)

--- spec/unit/provider/keystone_user/openstack_spec.rb
+++ spec/unit/provider/keystone_user/openstack_spec.rb
@@ -125,8 +125,8 @@ username="foo"
 ')
         provider.class.expects(:openstack)
                       .with('user role', 'list', '--quiet', '--format', 'csv', [['foo', '--project', 'foo', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
-                      .returns('"Project"
-"foo"
+                      .returns('"ID","Name","Project","User"
+"1cb05cfed7c24279be884ba4f6520262","foo","foo","foo"
 ')
         tenant = provider.tenant
         expect(tenant).to eq('foo')
@@ -139,8 +139,8 @@ username="foo"
                         .with('user', 'set', [['foo', '--project', 'bar', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
            provider.class.expects(:openstack)
                          .with('user role', 'list', '--quiet', '--format', 'csv', [['foo', '--project', 'bar', '--os-username', 'test', '--os-password', 'abc123', '--os-tenant-name', 'foo', '--os-auth-url', 'http://127.0.0.1:5000/v2.0']])
-                         .returns('"Project"
-"bar"
+                         .returns('"ID","Name","Project","User"
+"1cb05cfed7c24279be884ba4f6520262","foo","foo","foo"
 ')
           provider.tenant=('bar')
         end
