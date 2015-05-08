e8a09f76075b8d780e2f6b1b2d8fb3a19f6f94ee
Emilien Macchi <emilien@redhat.com>
2015-05-08 11:55:13 -0400

    Bring Redhat support to acceptance tests
    OpenStack Infra has jobs to run this on both Ubuntu Trusty and CentOS7.
    
    * Add minitest to Gemfile (dependency to run beaker on centos - see
      http://projects.theforeman.org/issues/2650 for details)
    * separate nodepool files to have trusty & centos7 support in OS infra
    * basic_keystone_spec: add case for repo configuration and support
      RH systems.
    
    Change-Id: Ia2e91512c097c3aa1cff9bf7f868ae08f86a44df
    Closes-bug: #1444736

--- spec/acceptance/basic_keystone_spec.rb
+++ spec/acceptance/basic_keystone_spec.rb
@@ -9,10 +9,31 @@ describe 'basic keystone server with resources' do
       Exec { logoutput => 'on_failure' }
 
       # Common resources
-      include ::apt
-      class { '::openstack_extras::repo::debian::ubuntu':
-         release         => 'kilo',
-         package_require => true,
+      case $::osfamily {
+        'Debian': {
+          include ::apt
+          class { '::openstack_extras::repo::debian::ubuntu':
+            release => 'kilo',
+            package_require => true,
+          }
+        }
+        'RedHat': {
+          class { '::openstack_extras::repo::redhat::redhat':
+            # Kilo is not GA yet, so let's use the testing repo
+            manage_rdo => false,
+            repo_hash  => {
+              'rdo-kilo-testing' => {
+                'baseurl'  => 'https://repos.fedorapeople.org/repos/openstack/openstack-kilo/testing/el7/',
+                # packages are not GA so not signed
+                'gpgcheck' => '0',
+                'priority' => 97,
+              },
+            },
+          }
+        }
+        default: {
+          fail("Unsupported osfamily (${::osfamily})")
+        }
       }
 
       class { '::mysql::server': }
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -30,7 +30,6 @@ RSpec.configure do |c|
       on host, puppet('module','install','dprince/qpid'), { :acceptable_exit_codes => 0 }
       on host, puppet('module','install','duritong/sysctl'), { :acceptable_exit_codes => 0 }
       on host, puppet('module','install','puppetlabs-inifile'), { :acceptable_exit_codes => 0 }
-      on host, puppet('module','install','stahnma-epel'), { :acceptable_exit_codes => 0 }
       on host, puppet('module','install','puppetlabs-rabbitmq'), { :acceptable_exit_codes => 0 }
       on host, puppet('module','install','puppetlabs-apache'), { :acceptable_exit_codes => 0 }
 
