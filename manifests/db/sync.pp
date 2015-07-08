ea12fb2d54ff22b4029112910a742316f1189c91
Mike Dorman <mdorman@godaddy.com>
2015-07-08 13:38:00 -0600

    Allow customization of db sync command line
    Add $extra_params parameter to keystone::db::sync class to allow
    end-users to add command line parameters to the db_sync command.
    
    Change-Id: I3965a4d0d4974e7ee925c99207d4d6d4510a517f
    Closes-bug: 1472740

--- manifests/db/sync.pp
+++ manifests/db/sync.pp
@@ -1,8 +1,20 @@
 #
 # Class to execute "keystone-manage db_sync
 #
-class keystone::db::sync {
+# == Parameters
+#
+# [*extra_params*]
+#   (optional) String of extra command line parameters to append
+#   to the keystone-manage db_sync command.  These will be
+#   inserted in the command line between 'keystone-manage' and
+#   'db_sync' in the command line.
+#   Defaults to ''
+#
+class keystone::db::sync(
+  $extra_params  = undef,
+) {
   exec { 'keystone-manage db_sync':
+    command     => "keystone-manage ${extra_params} db_sync",
     path        => '/usr/bin',
     user        => 'keystone',
     refreshonly => true,
