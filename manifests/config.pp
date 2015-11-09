50b4bfd44969c33bc16b40fe5907ee42b456ca5f
Mykyta Karpin <mkarpin@mirantis.com>
2015-11-09 12:45:03 +0200

    Add ability to configure paste ini options using config class
      It is needed in order to have possibility to add custom api options
      without direct usage of config providers in manifests.
    
    Change-Id: I3e4a2ba175d6974ba4c4fb170aa663d15cb8467a

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,14 +17,20 @@
 #     DEFAULT/bar:
 #       value: barValue
 #
+# [*keystone_paste_ini*]
+#   (optional) Allow configuration of /etc/keystone/keystone-paste.ini options.
+#
 #   NOTE: The configuration MUST NOT be already handled by this module
 #   or Puppet catalog compilation will fail with duplicate resources.
 #
 class keystone::config (
   $keystone_config  = {},
+  $keystone_paste_ini = {},
 ) {
 
   validate_hash($keystone_config)
+  validate_hash($keystone_paste_ini)
 
   create_resources('keystone_config', $keystone_config)
+  create_resources('keystone_paste_ini', $keystone_paste_ini)
 }
