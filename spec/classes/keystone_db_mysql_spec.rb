7c754b89b54c81b58a81d52b133141c56c2103b3
Nate Potter <nathaniel.potter@intel.com>
2015-11-16 17:10:17 +0000

    Add test coverage for keystone::db::mysql
    Currently most of the tests in keystone_db_mysql_spec.rb aren't checking
    anything, they're just setting up the framework. This patch will add test
    coverage for mysql.
    
    Change-Id: Ieb497d7ebeb0ddc145a53548500b02fbc10399ca

--- spec/classes/keystone_db_mysql_spec.rb
+++ spec/classes/keystone_db_mysql_spec.rb
@@ -15,18 +15,18 @@ describe 'keystone::db::mysql' do
 
   let :params do
     {
-      'password'      => 'keystone_default_password',
+      :password      => 'keystone_default_password',
     }
   end
 
   describe 'with only required params' do
     it { is_expected.to contain_openstacklib__db__mysql('keystone').with(
-      'user'          => 'keystone',
-      'password_hash' => '*B552157B14BCEDDCEAA06767A012F31BDAA9CE3D',
-      'dbname'        => 'keystone',
-      'host'          => '127.0.0.1',
-      'charset'       => 'utf8',
-      :collate        => 'utf8_general_ci',
+      :user          => 'keystone',
+      :password_hash => '*B552157B14BCEDDCEAA06767A012F31BDAA9CE3D',
+      :dbname        => 'keystone',
+      :host          => '127.0.0.1',
+      :charset       => 'utf8',
+      :collate       => 'utf8_general_ci',
     )}
   end
 
@@ -38,6 +38,16 @@ describe 'keystone::db::mysql' do
       }
     end
 
+    it { is_expected.to contain_openstacklib__db__mysql('keystone').with(
+      :user          => 'keystone',
+      :password_hash => '*706BFA85E15D0C1D8467D0D81D784F6A04CE4ABB',
+      :dbname        => 'keystone',
+      :host          => '127.0.0.1',
+      :charset       => 'utf8',
+      :collate       => 'utf8_general_ci',
+      :allowed_hosts => ['127.0.0.1','%'],
+    )}
+
   end
   describe "overriding allowed_hosts param to string" do
     let :params do
@@ -46,6 +56,16 @@ describe 'keystone::db::mysql' do
         :allowed_hosts  => '192.168.1.1'
       }
     end
+
+    it { is_expected.to contain_openstacklib__db__mysql('keystone').with(
+      :user          => 'keystone',
+      :password_hash => '*47651CDAAB340A79CC838378072877FFFBF0B239',
+      :dbname        => 'keystone',
+      :host          => '127.0.0.1',
+      :charset       => 'utf8',
+      :collate       => 'utf8_general_ci',
+      :allowed_hosts => '192.168.1.1',
+    )}
 
   end
 
@@ -56,6 +76,16 @@ describe 'keystone::db::mysql' do
         :allowed_hosts  => '127.0.0.1'
       }
     end
+
+    it { is_expected.to contain_openstacklib__db__mysql('keystone').with(
+      :user          => 'keystone',
+      :password_hash => '*47651CDAAB340A79CC838378072877FFFBF0B239',
+      :dbname        => 'keystone',
+      :host          => '127.0.0.1',
+      :charset       => 'utf8',
+      :collate       => 'utf8_general_ci',
+      :allowed_hosts => '127.0.0.1',
+    )}
 
   end
 
