ff3c84d3cc519ff7d6e5849bde4705165d93f390
MikeG451 <mg6596@att.com>
2016-04-15 14:02:37 -0700

    Add Unit Tests for keystone_config type
    Change-Id: Ieb34b1d9ff0d94e196d0ae4e07465fb7dd719b62
    Partial-bug: #1440401

--- spec/unit/type/keystone_config_spec.rb
+++ spec/unit/type/keystone_config_spec.rb
@@ -19,6 +19,51 @@ describe 'Puppet::Type.type(:keystone_config)' do
     @keystone_config = Puppet::Type.type(:keystone_config).new(:name => 'DEFAULT/foo', :value => 'bar')
   end
 
+ it 'should require a name' do
+    expect {
+      Puppet::Type.type(:keystone_config).new({})
+    }.to raise_error(Puppet::Error, 'Title or name must be provided')
+  end
+
+  it 'should not expect a name with whitespace' do
+    expect {
+      Puppet::Type.type(:keystone_config).new(:name => 'f oo')
+    }.to raise_error(Puppet::Error, /Parameter name failed/)
+  end
+
+  it 'should fail when there is no section' do
+    expect {
+      Puppet::Type.type(:keystone_config).new(:name => 'foo')
+    }.to raise_error(Puppet::Error, /Parameter name failed/)
+  end
+
+  it 'should not require a value when ensure is absent' do
+    Puppet::Type.type(:keystone_config).new(:name => 'DEFAULT/foo', :ensure => :absent)
+  end
+
+  it 'should accept a valid value' do
+    @keystone_config[:value] = 'bar'
+    expect(@keystone_config[:value]).to eq('bar')
+  end
+
+  it 'should not accept a value with whitespace' do
+    @keystone_config[:value] = 'b ar'
+    expect(@keystone_config[:value]).to eq('b ar')
+  end
+
+  it 'should accept valid ensure values' do
+    @keystone_config[:ensure] = :present
+    expect(@keystone_config[:ensure]).to eq(:present)
+    @keystone_config[:ensure] = :absent
+    expect(@keystone_config[:ensure]).to eq(:absent)
+  end
+
+  it 'should not accept invalid ensure values' do
+    expect {
+      @keystone_config[:ensure] = :latest
+    }.to raise_error(Puppet::Error, /Invalid value/)
+  end
+
   it 'should autorequire the package that install the file' do
     catalog = Puppet::Resource::Catalog.new
     package = Puppet::Type.type(:package).new(:name => 'keystone')
