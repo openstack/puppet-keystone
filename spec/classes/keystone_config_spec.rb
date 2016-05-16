8832518b9f841c66c1061e6ee6bafbe6ef8dd6a5
ZhongShengping <chdzsp@163.com>
2016-05-16 09:18:50 +0800

    Add unit test for keystone::config
    There is no spect test for keystone::config, so add it.
    
    Change-Id: Iebc4c6fbccabf118cf5744a124df9c5a97df2618
    Closes-Bug: 1581282

---
+++ spec/classes/keystone_config_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe 'keystone::config' do
+
+  let(:config_hash) do {
+    'DEFAULT/foo' => { 'value'  => 'fooValue' },
+    'DEFAULT/bar' => { 'value'  => 'barValue' },
+    'DEFAULT/baz' => { 'ensure' => 'absent' }
+  }
+  end
+
+  shared_examples_for 'keystone_config' do
+    let :params do
+      { :keystone_config => config_hash }
+    end
+
+    it 'configures arbitrary keystone-config configurations' do
+      is_expected.to contain_keystone_config('DEFAULT/foo').with_value('fooValue')
+      is_expected.to contain_keystone_config('DEFAULT/bar').with_value('barValue')
+      is_expected.to contain_keystone_config('DEFAULT/baz').with_ensure('absent')
+    end
+  end
+
+  shared_examples_for 'keystone_paste_ini' do
+    let :params do
+      { :keystone_paste_ini => config_hash }
+    end
+
+    it 'configures arbitrary keystone-paste-ini configurations' do
+      is_expected.to contain_keystone_paste_ini('DEFAULT/foo').with_value('fooValue')
+      is_expected.to contain_keystone_paste_ini('DEFAULT/bar').with_value('barValue')
+      is_expected.to contain_keystone_paste_ini('DEFAULT/baz').with_ensure('absent')
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_configures 'keystone_config'
+      it_configures 'keystone_paste_ini'
+    end
+  end
+end
