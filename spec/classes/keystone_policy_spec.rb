067b75baf21d1904440eb58005ce426a1599ff76
iberezovskiy <iberezovskiy@mirantis.com>
2016-06-07 19:48:36 +0300

    Fix facts for policy test
    Change-Id: I7a03dc9ec586dea530d6d17f8cda0237d7a2cc8d

--- spec/classes/keystone_policy_spec.rb
+++ spec/classes/keystone_policy_spec.rb
@@ -29,7 +29,7 @@ describe 'keystone::policy' do
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
-        facts.merge(OSDefaults.get_facts({:processorcount => 8}))
+        facts.merge!(OSDefaults.get_facts())
       end
 
       it_configures 'keystone policies'
