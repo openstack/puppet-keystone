60a114700dc7dab416e76c2868bdb849660544d1
Mykyta Karpin <mkarpin@mirantis.com>
2016-10-10 18:11:28 +0300

    Fix documentation for log_dir parameter
    Change-Id: I2e473b45f511d2edef608fc245f68d75de59e7f9
    Partial-Bug: #1600294

--- manifests/init.pp
+++ manifests/init.pp
@@ -305,7 +305,7 @@
 #
 # [*log_dir*]
 #   (optional) Directory where logs should be stored
-#   If set to boolean false, it will not log to any directory
+#   If set to $::os_service_default, it will not log to any directory
 #   Defaults to undef.
 #
 # [*log_file*]
--- manifests/logging.pp
+++ manifests/logging.pp
@@ -22,7 +22,7 @@
 #
 #  [*log_dir*]
 #    (optional) Directory where logs should be stored.
-#    If set to boolean false, it will not log to any directory.
+#    If set to $::os_service_default, it will not log to any directory.
 #    Defaults to '/var/log/keystone'
 #
 #  [*log_file*]
