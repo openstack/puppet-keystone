7edf617586d7791825b567e631e171e50db47ec3
ZhongShengping <chdzsp@163.com>
2020-08-26 11:53:17 +0800

    Add mysql_enable_ndb option
    Add mysql_enable_ndb parameter to select mysql storage engine.
    
    Change-Id: I2f6351c29e50070bc712879642fd509d9797ddb0
    Depends-On: https://review.opendev.org/#/c/748067
    Closes-Bug: #1892952

--- spec/classes/keystone_db_spec.rb
+++ spec/classes/keystone_db_spec.rb
@@ -14,6 +14,7 @@ describe 'keystone::db' do
         :retry_interval          => '<SERVICE DEFAULT>',
         :max_overflow            => '<SERVICE DEFAULT>',
         :pool_timeout            => '<SERVICE DEFAULT>',
+        :mysql_enable_ndb        => '<SERVICE DEFAULT>',
       )}
     end
 
@@ -27,6 +28,7 @@ describe 'keystone::db' do
           :database_max_retries             => '11',
           :database_max_overflow            => '21',
           :database_pool_timeout            => '21',
+          :mysql_enable_ndb                 => true,
           :database_retry_interval          => '11',
         }
       end
@@ -42,6 +44,7 @@ describe 'keystone::db' do
         :retry_interval          => '11',
         :max_overflow            => '21',
         :pool_timeout            => '21',
+        :mysql_enable_ndb        => true,
       )}
     end
   end
