bfba668cff60ca142ef587b969d0d5e5500fde33
Mikhail Stolyarov <mikhail.stolyarov@megafon.ru>
2020-08-28 15:54:12 +0300

    Add python version to package python-ldappool

--- manifests/ldap_backend.pp
+++ manifests/ldap_backend.pp
@@ -652,7 +652,7 @@ and \"${domain_dir_enabled}\" for identity/domain_config_dir"
   $domain = $name
 
   if $manage_packages {
-    ensure_resource('package', 'python-ldappool', {
+    ensure_resource('package', "python${::keystone::params::pyvers}-ldappool", {
       ensure  => $package_ensure,
       tag => ['openstack', 'keystone-package'],
     })
