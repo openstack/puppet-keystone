b4e486705b42a971ac324d7650e6e38bf1939110
Takashi Kajinami <tkajinam@redhat.com>
2021-02-01 08:10:23 +0900

    Add support for healthcheck middleware options
    Depends-on: https://review.opendev.org/772202
    Change-Id: I91a410c4814963d93c26abb529b96a6ffecd9248

---
+++ manifests/healthcheck.pp
@@ -0,0 +1,41 @@
+# == Define: keystone::healthcheck
+#
+# Configure oslo_middleware options in healthcheck section
+#
+# == Params
+#
+# [*detailed*]
+#   (Optional) Show more detailed information as part of the response.
+#   Defaults to $::os_service_default
+#
+# [*backends*]
+#   (Optional) Additional backends that can perform health checks and report
+#   that information back as part of a request.
+#   Defaults to $::os_service_default
+#
+# [*disable_by_file_path*]
+#   (Optional) Check the presense of a file to determine if an application
+#   is running on a port.
+#   Defaults to $::os_service_default
+#
+# [*disable_by_file_paths*]
+#   (Optional) Check the presense of a file to determine if an application
+#   is running on a port. Expects a "port:path" list of strings.
+#   Defaults to $::os_service_default
+#
+class keystone::healthcheck (
+  $detailed              = $::os_service_default,
+  $backends              = $::os_service_default,
+  $disable_by_file_path  = $::os_service_default,
+  $disable_by_file_paths = $::os_service_default,
+) {
+
+  include keystone::deps
+
+  oslo::healthcheck { 'keystone_config':
+    detailed              => $detailed,
+    backends              => $backends,
+    disable_by_file_path  => $disable_by_file_path,
+    disable_by_file_paths => $disable_by_file_paths,
+  }
+}
