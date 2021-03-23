1fe0e6b320d16e78b08e164495786921f38db4af
Thomas Goirand <zigo@debian.org>
2021-03-23 09:24:03 +0100

    Allow to configure policy_dirs
    This patch makes it possible to override the current service default,
    which is /etc/<service>/policy.d .
    
    Change-Id: Ifdf0f0808789fc82a8f53307c3c7e7676cbdc4b1

--- spec/classes/keystone_policy_spec.rb
+++ spec/classes/keystone_policy_spec.rb
@@ -7,6 +7,7 @@ describe 'keystone::policy' do
         :enforce_scope        => false,
         :enforce_new_defaults => false,
         :policy_path          => '/etc/keystone/policy.yaml',
+        :policy_dirs          => '/etc/keystone/policy.d',
         :policies             => {
           'context_is_admin' => {
             'key'   => 'context_is_admin',
@@ -28,6 +29,7 @@ describe 'keystone::policy' do
         :enforce_scope        => false,
         :enforce_new_defaults => false,
         :policy_file          => '/etc/keystone/policy.yaml',
+        :policy_dirs          => '/etc/keystone/policy.d',
       )
     end
   end
