4a0b48ca6c64395d2ad62b0be5ebe7ac110b82b7
Takashi Kajinami <tkajinam@redhat.com>
2021-06-12 23:33:28 +0900

    Fix leftover related to $pyvers variable
    This change removes a logic in unit tests assuming usage of $pyvers,
    which was left by 62d556fc63da83fbd56ea80c78e6eaac5f559cc0 which
    removed $pyvers.
    
    Change-Id: I6988ee097d61be78f065c79ce18dc2bac83ddb10

--- spec/defines/keystone_resource_authtoken_spec.rb
+++ spec/defines/keystone_resource_authtoken_spec.rb
@@ -214,15 +214,7 @@ describe 'keystone::resource::authtoken' do
         when 'Debian'
           memcache_package_name = 'python3-memcache'
         when 'RedHat'
-          if facts[:operatingsystem] == 'Fedora'
-            memcache_package_name = 'python3-memcached'
-          else
-            if facts[:operatingsystemmajrelease] > '7'
-              memcache_package_name = 'python3-memcached'
-            else
-              memcache_package_name = 'python-memcached'
-            end
-          end
+          memcache_package_name = 'python3-memcached'
         end
         {
           :memcache_package_name => memcache_package_name
