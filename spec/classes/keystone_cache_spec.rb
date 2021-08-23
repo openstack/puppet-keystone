068564b6c79d847ae928473263771ab98b68c78c
Takashi Kajinami <tkajinam@redhat.com>
2021-08-23 14:30:39 +0900

    Do not test details of oslo::cache
    ... but test interface of oslo::cache, to make these test cases robust
    for any change in oslo::cache.
    
    Change-Id: I9fae84582c1f96ab324527f1efafec1669c08693

--- spec/classes/keystone_cache_spec.rb
+++ spec/classes/keystone_cache_spec.rb
@@ -1,49 +1,51 @@
 require 'spec_helper'
 
 describe 'keystone::cache' do
-  shared_examples 'keystone::cache' do
+
+  let :params do
+    {}
+  end
+
+  shared_examples_for 'keystone::cache' do
 
     context 'with default parameters' do
-      let :params do
-        {}
-      end
-
-      it 'configure cache default params' do
+      it 'configures cache' do
         is_expected.to contain_keystone_config('memcache/dead_retry').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_keystone_config('memcache/pool_maxsize').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_keystone_config('memcache/pool_unused_timeout').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_keystone_config('memcache/socket_timeout').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_keystone_config('token/caching').with_value('<SERVICE DEFAULT>')
 
-        is_expected.to contain_keystone_config('cache/config_prefix').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/expiration_time').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/backend').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/backend_argument').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/proxies').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/enabled').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/debug_cache_backend').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/memcache_servers').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/memcache_dead_retry').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/memcache_socket_timeout').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/memcache_pool_unused_timeout').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/memcache_servers').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/tls_enabled').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/tls_cafile').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/tls_certfile').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/tls_keyfile').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_keystone_config('cache/tls_allowed_ciphers').with_value('<SERVICE DEFAULT>')
-
-        is_expected.to contain_oslo__cache('keystone_config').with_manage_backend_package(true)
+        is_expected.to contain_oslo__cache('keystone_config').with(
+          :config_prefix                        => '<SERVICE DEFAULT>',
+          :expiration_time                      => '<SERVICE DEFAULT>',
+          :backend                              => '<SERVICE DEFAULT>',
+          :backend_argument                     => '<SERVICE DEFAULT>',
+          :proxies                              => '<SERVICE DEFAULT>',
+          :enabled                              => '<SERVICE DEFAULT>',
+          :debug_cache_backend                  => '<SERVICE DEFAULT>',
+          :memcache_servers                     => ['<SERVICE DEFAULT>'],
+          :memcache_dead_retry                  => '<SERVICE DEFAULT>',
+          :memcache_socket_timeout              => '<SERVICE DEFAULT>',
+          :memcache_pool_maxsize                => '<SERVICE DEFAULT>',
+          :memcache_pool_unused_timeout         => '<SERVICE DEFAULT>',
+          :memcache_pool_connection_get_timeout => '<SERVICE DEFAULT>',
+          :tls_enabled                          => '<SERVICE DEFAULT>',
+          :tls_cafile                           => '<SERVICE DEFAULT>',
+          :tls_certfile                         => '<SERVICE DEFAULT>',
+          :tls_keyfile                          => '<SERVICE DEFAULT>',
+          :tls_allowed_ciphers                  => '<SERVICE DEFAULT>',
+          :manage_backend_package               => true,
+        )
       end
     end
 
-    context 'with specific values' do
+    context 'with overridden parameters' do
       let :params do
         {
           :config_prefix                        => 'prefix',
-          :expiration_time                      => '3600',
-          :backend                              => 'dogpile.cache.memcached',
-          :backend_argument                     => ['url:SERVER1:12211'],
+          :expiration_time                      => 3600,
+          :backend                              => 'oslo_cache.memcache_pool',
           :proxies                              => ['proxy01:8888', 'proxy02:8888'],
           :enabled                              => true,
           :debug_cache_backend                  => false,
@@ -53,45 +55,42 @@ describe 'keystone::cache' do
           :memcache_pool_maxsize                => '10',
           :memcache_pool_unused_timeout         => '120',
           :memcache_pool_connection_get_timeout => '360',
+          :tls_enabled                          => false,
           :manage_backend_package               => false,
-          :token_caching                        => 'true',
-          :tls_enabled                          => 'false',
-          :tls_cafile                           => nil,
-          :tls_certfile                         => nil,
-          :tls_keyfile                          => nil,
-          :tls_allowed_ciphers                  => nil,
+          :token_caching                        => true,
         }
       end
 
-      it 'configure cache params' do
+      it 'configures cache' do
         is_expected.to contain_keystone_config('memcache/dead_retry').with_value('60')
         is_expected.to contain_keystone_config('memcache/pool_maxsize').with_value('10')
         is_expected.to contain_keystone_config('memcache/pool_unused_timeout').with_value('120')
         is_expected.to contain_keystone_config('memcache/socket_timeout').with_value('300.0')
-        is_expected.to contain_keystone_config('token/caching').with_value('true')
+        is_expected.to contain_keystone_config('token/caching').with_value(true)
 
-        is_expected.to contain_keystone_config('cache/config_prefix').with_value('prefix')
-        is_expected.to contain_keystone_config('cache/expiration_time').with_value('3600')
-        is_expected.to contain_keystone_config('cache/backend').with_value('dogpile.cache.memcached')
-        is_expected.to contain_keystone_config('cache/backend_argument').with_value('url:SERVER1:12211')
-        is_expected.to contain_keystone_config('cache/proxies').with_value('proxy01:8888,proxy02:8888')
-        is_expected.to contain_keystone_config('cache/enabled').with_value('true')
-        is_expected.to contain_keystone_config('cache/debug_cache_backend').with_value('false')
-        is_expected.to contain_keystone_config('cache/memcache_servers').with_value('memcached01:11211,memcached02:11211')
-        is_expected.to contain_keystone_config('cache/memcache_dead_retry').with_value('60')
-        is_expected.to contain_keystone_config('cache/memcache_socket_timeout').with_value('300.0')
-        is_expected.to contain_keystone_config('cache/memcache_pool_maxsize').with_value('10')
-        is_expected.to contain_keystone_config('cache/memcache_pool_unused_timeout').with_value('120')
-        is_expected.to contain_keystone_config('cache/tls_enabled').with_value('false')
-        is_expected.to contain_keystone_config('cache/tls_cafile').with_value('nil')
-        is_expected.to contain_keystone_config('cache/tls_certfile').with_value('nil')
-        is_expected.to contain_keystone_config('cache/tls_keyfile').with_value('nil')
-        is_expected.to contain_keystone_config('cache/tls_allowed_ciphers').with_value('nil')
-
-        is_expected.to contain_oslo__cache('keystone_config').with_manage_backend_package(false)
+        is_expected.to contain_oslo__cache('keystone_config').with(
+          :config_prefix                        => 'prefix',
+          :expiration_time                      => 3600,
+          :backend                              => 'oslo_cache.memcache_pool',
+          :backend_argument                     => '<SERVICE DEFAULT>',
+          :proxies                              => ['proxy01:8888', 'proxy02:8888'],
+          :enabled                              => true,
+          :debug_cache_backend                  => false,
+          :memcache_servers                     => ['memcached01:11211', 'memcached02:11211'],
+          :memcache_dead_retry                  => '60',
+          :memcache_socket_timeout              => '300.0',
+          :memcache_pool_maxsize                => '10',
+          :memcache_pool_unused_timeout         => '120',
+          :memcache_pool_connection_get_timeout => '360',
+          :tls_enabled                          => false,
+          :tls_cafile                           => '<SERVICE DEFAULT>',
+          :tls_certfile                         => '<SERVICE DEFAULT>',
+          :tls_keyfile                          => '<SERVICE DEFAULT>',
+          :tls_allowed_ciphers                  => '<SERVICE DEFAULT>',
+          :manage_backend_package               => false,
+        )
       end
     end
-
   end
 
   on_supported_os({
@@ -102,7 +101,8 @@ describe 'keystone::cache' do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_behaves_like 'keystone::cache'
+      it_configures 'keystone::cache'
     end
   end
+
 end
