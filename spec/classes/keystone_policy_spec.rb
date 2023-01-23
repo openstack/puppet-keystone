caa8c1a5f6a95ed5ea0fd47bd7a17848bd265a94
Takashi Kajinami <tkajinam@redhat.com>
2023-01-23 14:25:55 +0900

    Expose policy_default_rule
    The option has been managed by the underlying puppet-oslo module but
    has not been configurable. This introduces the parameter to customize
    the option.
    
    Change-Id: Ia91f1558f6f5b77f3fcd77149dc61cafd621308e

--- spec/classes/keystone_policy_spec.rb
+++ spec/classes/keystone_policy_spec.rb
@@ -9,6 +9,7 @@ describe 'keystone::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_path          => '/etc/keystone/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/keystone/policy.d',
           :policies             => {
             'context_is_admin' => {
@@ -37,6 +38,7 @@ describe 'keystone::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_file          => '/etc/keystone/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/keystone/policy.d',
         )
       end
