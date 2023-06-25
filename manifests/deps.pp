74468f0c675005b3791d14f0e7eeb678fd0c413a
Tobias Urdin <tobias.urdin@binero.com>
2023-06-25 23:51:19 +0200

    Add per module policy service refresh
    Updating the policies for this project should only
    refresh the services that reads it.
    
    Change-Id: I80117d1c7ab1bd9642a6c3d416c6683ae024894a
    (cherry picked from commit 9fee3031a3f7bcb3427d2570cca5f150d9068db5)

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -39,7 +39,7 @@ class keystone::deps {
 
   # policy config should occur in the config block also.
   Anchor['keystone::config::begin']
-  -> Openstacklib::Policy<||>
+  -> Openstacklib::Policy<| tag == 'keystone' |>
   ~> Anchor['keystone::config::end']
 
   # Support packages need to be installed in the install phase, but we don't
