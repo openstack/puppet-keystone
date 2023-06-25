ffd91c6aacbd53f95d75af0b3a47dda0c247ed56
Tobias Urdin <tobias.urdin@binero.com>
2023-06-25 23:51:19 +0200

    Add per module policy service refresh
    Updating the policies for this project should only
    refresh the services that reads it.
    
    Change-Id: I80117d1c7ab1bd9642a6c3d416c6683ae024894a
    (cherry picked from commit 9fee3031a3f7bcb3427d2570cca5f150d9068db5)
    (cherry picked from commit 74468f0c675005b3791d14f0e7eeb678fd0c413a)
    (cherry picked from commit eed08fb49b450a7aa441cede453e7ef0aa160d81)

--- spec/classes/keystone_policy_spec.rb
+++ spec/classes/keystone_policy_spec.rb
@@ -32,6 +32,7 @@ describe 'keystone::policy' do
           :file_group   => 'keystone',
           :file_format  => 'yaml',
           :purge_config => false,
+          :tag          => 'keystone',
         )
         is_expected.to contain_oslo__policy('keystone_config').with(
           :enforce_scope        => false,
@@ -61,6 +62,7 @@ describe 'keystone::policy' do
           :file_group   => 'keystone',
           :file_format  => 'yaml',
           :purge_config => true,
+          :tag          => 'keystone',
         )
         is_expected.to contain_oslo__policy('keystone_config').with(
           :enforce_scope        => false,
