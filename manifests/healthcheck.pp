b370f838433709d18049a8644fb247643453949c
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-01-17 15:23:31 +0900

    healthcheck: Expose allowed_source_ranges
    ... which was added to puppet-oslo recently.
    
    Depends-on: https://review.opendev.org/905557
    Change-Id: I7a239c556c9e5615ed7668ae2d5ad6c0bf73b624

--- manifests/healthcheck.pp
+++ manifests/healthcheck.pp
@@ -13,6 +13,11 @@
 #   that information back as part of a request.
 #   Defaults to $facts['os_service_default']
 #
+# [*allowed_source_ranges*]
+#   (Optional) A list of network addresses to limit source ip allowed to access
+#   healthcheck information.
+#   Defaults to $facts['os_service_default']
+#
 # [*disable_by_file_path*]
 #   (Optional) Check the presence of a file to determine if an application
 #   is running on a port.
@@ -26,6 +31,7 @@
 class keystone::healthcheck (
   $detailed              = $facts['os_service_default'],
   $backends              = $facts['os_service_default'],
+  $allowed_source_ranges = $facts['os_service_default'],
   $disable_by_file_path  = $facts['os_service_default'],
   $disable_by_file_paths = $facts['os_service_default'],
 ) {
@@ -35,6 +41,7 @@ class keystone::healthcheck (
   oslo::healthcheck { 'keystone_config':
     detailed              => $detailed,
     backends              => $backends,
+    allowed_source_ranges => $allowed_source_ranges,
     disable_by_file_path  => $disable_by_file_path,
     disable_by_file_paths => $disable_by_file_paths,
   }
