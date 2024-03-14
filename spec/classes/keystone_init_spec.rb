da2bc869a314ae10b60d953b1dbd862c59870160
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-03-14 09:34:06 +0900

    Expose rabbit_transient_quorum_queue
    Depends-on: https://review.opendev.org/911021
    Change-Id: Ibe989d7a9bb10d6dff72b001c0e689bb029960b5

--- spec/classes/keystone_init_spec.rb
+++ spec/classes/keystone_init_spec.rb
@@ -79,6 +79,7 @@ describe 'keystone' do
           :heartbeat_in_pthread               => '<SERVICE DEFAULT>',
           :amqp_durable_queues                => '<SERVICE DEFAULT>',
           :rabbit_quorum_queue                => '<SERVICE DEFAULT>',
+          :rabbit_transient_quorum_queue      => '<SERVICE DEFAULT>',
           :rabbit_quorum_delivery_limit       => '<SERVICE DEFAULT>',
           :rabbit_quorum_max_memory_length    => '<SERVICE DEFAULT>',
           :rabbit_quorum_max_memory_bytes     => '<SERVICE DEFAULT>',
