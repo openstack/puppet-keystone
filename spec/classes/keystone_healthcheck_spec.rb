18ed4c0d5112dd59baa78dddc1ec631499479b1d
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-07-23 19:20:19 +0900

    Add support for [healthcheck] enable_by_file_paths
    Depends-on: https://review.opendev.org/923648
    Change-Id: Iddf7bcfa4a423459a4a007ca51387a4e86774029

--- spec/classes/keystone_healthcheck_spec.rb
+++ spec/classes/keystone_healthcheck_spec.rb
@@ -17,6 +17,7 @@ describe 'keystone::healthcheck' do
           :ignore_proxied_requests => '<SERVICE DEFAULT>',
           :disable_by_file_path    => '<SERVICE DEFAULT>',
           :disable_by_file_paths   => '<SERVICE DEFAULT>',
+          :enable_by_file_paths    => '<SERVICE DEFAULT>',
         )
       end
     end
@@ -30,6 +31,7 @@ describe 'keystone::healthcheck' do
           :ignore_proxied_requests => false,
           :disable_by_file_path    => '/etc/keystone/healthcheck/disabled',
           :disable_by_file_paths   => ['5000:/etc/keystone/healthcheck/disabled'],
+          :enable_by_file_paths    => ['/etc/keystone/healthcheck/enabled'],
         }
       end
 
@@ -41,6 +43,7 @@ describe 'keystone::healthcheck' do
           :ignore_proxied_requests => false,
           :disable_by_file_path    => '/etc/keystone/healthcheck/disabled',
           :disable_by_file_paths   => ['5000:/etc/keystone/healthcheck/disabled'],
+          :enable_by_file_paths    => ['/etc/keystone/healthcheck/enabled'],
         )
       end
     end
