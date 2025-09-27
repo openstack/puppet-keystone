4ecc8550a79b2739b5fc28d98d882a58290f094b
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-09-27 23:55:44 +0900

    Import base parameter types for ::policy
    Import the parameter types from openstacklib::policy so that parameter
    types are validated at module level, instead of internal resource
    call.
    
    Change-Id: I461f4da59cad3ba27acd676a262c93bb8d1d18a2
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -46,13 +46,13 @@
 #    Defaults to false.
 #
 class keystone::policy (
-  $enforce_scope        = $facts['os_service_default'],
-  $enforce_new_defaults = $facts['os_service_default'],
-  Hash $policies        = {},
-  $policy_path          = '/etc/keystone/policy.yaml',
-  $policy_default_rule  = $facts['os_service_default'],
-  $policy_dirs          = $facts['os_service_default'],
-  Boolean $purge_config = false,
+  $enforce_scope                    = $facts['os_service_default'],
+  $enforce_new_defaults             = $facts['os_service_default'],
+  Openstacklib::Policies $policies  = {},
+  Stdlib::Absolutepath $policy_path = '/etc/keystone/policy.yaml',
+  $policy_default_rule              = $facts['os_service_default'],
+  $policy_dirs                      = $facts['os_service_default'],
+  Boolean $purge_config             = false,
 ) {
   include keystone::deps
   include keystone::params
